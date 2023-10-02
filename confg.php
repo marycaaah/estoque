d464acc0a703727478b3c3f31b6d642a0deda8d6
marycaaah <106408633+marycaaah@users.noreply.github.com>
2023-10-02 20:36:10 -0300

    Add files via upload

---
+++ confg.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'estoque';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
